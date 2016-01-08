6d91d29e62522a0f27a228dc19308410172c9d35
Isaiah Smith <smithij@rose-hulman.edu>
2016-01-07 21:47:05 -0500

    Daniel: Setting up the "stream" for decorating

--- src/project/DotEncoder.java
+++ src/project/DotEncoder.java
@@ -5,9 +5,13 @@ import java.util.List;
 public class DotEncoder implements IEncoder {
 
 	@Override
-	public StringBuilder encode(List<ClassBuilder> Classes) {
-		return null;
-		
+	public StringBuilder encode(List<ClassBuilder> Classes){
+		IDot dot = new StandardDot();
+		//dot = new DotClassDecorator(dot, Classes);
+		//dot = new DotInheritanceDecorator(dot, /*Inheritance graph*/);
+		//dot = new DotImplementsDecorator(dot, /*Implements graph*/);
+		return dot.getDot();
 	}
+	
 
 }
