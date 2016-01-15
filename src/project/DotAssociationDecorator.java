3df20354d2062114a42d8c18e267f32f435742bd
Isaiah Smith <smithij@rose-hulman.edu>
2016-01-14 22:08:35 -0500

    getting only correct classes for association now

--- src/project/DotAssociationDecorator.java
+++ src/project/DotAssociationDecorator.java
@@ -3,6 +3,8 @@ package project;
 import java.util.List;
 import java.util.Map;
 
+import org.objectweb.asm.Type;
+
 public class DotAssociationDecorator extends DotDecorator {
 
 	ADot toBeDecorated;
@@ -17,17 +19,23 @@ public class DotAssociationDecorator extends DotDecorator {
 		StringBuilder temp = new StringBuilder("edge [ \n\t\tarrowhead = \"vee\" \n\tstyle = \"filled\"]\n\t");
 		for(ClassBuilder c : classes){
 			for(Map<String, String> field : c.fields){
-				if(field.get("Type").getClass().isLocalClass()){
-					String left = c.name;
-					String right = field.get("Type");
-					String[] leftSplit = left.split("/");
-					String[] rightSplit = right.split("/");
-					left = leftSplit[leftSplit.length - 1];
-					right = rightSplit[rightSplit.length - 1];
-					String[] periodSplit = right.split("\\.");
-					right = periodSplit[periodSplit.length - 1];
-					temp.append(" " + left + " -> " + right+"\n\t");					
+				String left = c.name;
+				String right = field.get("Type");
+				String[] leftSplit = left.split("/");
+				String[] rightSplit = right.split("/");
+				left = leftSplit[leftSplit.length - 1];
+				right = rightSplit[rightSplit.length - 1];
+				try {
+					Class<?> rightClass = Class.forName(right);
+					if(!rightClass.toString().split(" ")[1].split("\\.")[0].equals("java")){
+						String[] periodSplit = right.split("\\.");
+						right = periodSplit[periodSplit.length - 1];
+						temp.append(" " + left + " -> " + right+"\n\t");											
+					}
+				} catch (ClassNotFoundException e) {
+//					e.printStackTrace();
 				}
+				
 			}
 		}
 		return temp;
