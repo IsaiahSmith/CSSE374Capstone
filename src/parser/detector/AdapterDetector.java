115af80c6a2fe6cc73884654f8297709c223fadd
unknown <lehmandr@LEHMANDR-1.rose-hulman.edu>
2016-01-29 06:13:23 -0500

    wrote stuff to detect Singletons. I think

--- src/parser/detector/AdapterDetector.java
+++ src/parser/detector/AdapterDetector.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.IFile;
+import model.IInnerNode;
+import model.IMethod;
+import model.INode;
 import model.IPattern;
+import nodes.Pattern;
 
 public class AdapterDetector extends Detector{
 	public static String PATTERN = "Adapter";
@@ -15,7 +19,30 @@ public class AdapterDetector extends Detector{
 	
 	@Override
 	public List<IPattern> detect() {
-		// TODO: implement this
-		return new ArrayList<IPattern>();
+		List<IPattern> patterns = new ArrayList<IPattern>();
+		for(IFile file:this.files) {
+			if(this.isSingleton(file)) {
+				IPattern pattern = new Pattern(PATTERN);
+				pattern.addNode(file);
+				patterns.add(pattern);
+			}
+		}
+		
+		return patterns;
+	}
+	
+	private boolean isSingleton(IFile file) {
+		boolean allPrivateConstructors = true;
+		boolean hasPrivateInstance = false;
+		for(IInnerNode field:file.getFields())
+			if(field.getType().equals(file.getName()) && field.getAccessLevel().equals("private"))
+				hasPrivateInstance = true;
+		for(IMethod method:file.getMethods()) {
+			if(method.getName().equals("<init>")){
+				if(!method.getType().equals("private"))
+					allPrivateConstructors = false;
+			}
+		}
+		return allPrivateConstructors && hasPrivateInstance;
 	}
 }
