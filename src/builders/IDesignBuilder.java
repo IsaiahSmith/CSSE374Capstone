c38c3c222c271ee3449a2049459e1c07d5431951
Isaiah Smith <smithij@rose-hulman.edu>
2016-01-29 00:21:49 -0500

    updated UML and cleaned up project

--- src/builders/IDesignBuilder.java
+++
@@ -1,9 +0,0 @@
-package builders;
-
-import java.io.IOException;
-
-import model.IModel;
-
-public interface IDesignBuilder {
-	public IModel build();
-}
--- src/parser/linker/InheritanceLinker.java
+++ src/parser/linker/InheritanceLinker.java
@@ -1,4 +1,4 @@
-package arrowDesignBuilder;
+package parser.linker;
 
 import java.util.ArrayList;
 import java.util.List;
